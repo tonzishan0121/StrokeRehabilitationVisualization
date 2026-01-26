889a10fabfd67e419db66d5f674452abbfe54964
tonzishan0121 <toninni@foxmail.com>
2026-01-27 07:06:31 +0800

    feat(patient-info): 调整患者信息展示布局并添加新字段 ``` - 修改SCSS样式：调整info-box布局，将flex方向改为row wrap，   对齐方式改为flex-start，增加字体大小和间距 - 更新PatientInfo组件：移除住院号显示，添加height、weight、   research_center等新字段到patientInfo数据结构 - 优化代码格式：统一代码缩进和空格规范 ```

--- src/components/PatientInfo.vue
+++ src/components/PatientInfo.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="info-item">
-    <span style="font-size:x-large;">姓名：{{patientInfo.name}}</span>
-    <span style="font-size:x-large;">性别：{{patientInfo.gender}}</span>
-    <span style="font-size:x-large;">年龄：{{patientInfo.age}}</span>
-    <span style="font-size:x-large;">住院号：{{patientInfo.hospitalNumber}}</span>
+    <span style="font-size:x-large;">姓名：{{ patientInfo.name }}</span>
+    <span style="font-size:x-large;">性别：{{ patientInfo.gender }}</span>
+    <span style="font-size:x-large;">年龄：{{ patientInfo.age }}</span>
     <span style="font-size:x-large;">症状：{{patientInfo.condition}}</span>
-    <span style="font-size:x-large;">科室：重症监护室</span>
   </div>
 </template>
 
@@ -18,13 +16,16 @@ const patientInfoInject = inject('patient_info');
 // 修改: 正确处理ref值的访问
 const patientInfo = computed(() => {
   // 当inject的值存在且有value属性时，返回value，否则返回默认对象
-    return patientInfoInject?.value || {
-      name: '',
-      gender: '',
-      age: '',
-      hospitalNumber: '',
-      condition: ''
-    };
+  return patientInfoInject?.value || {
+    name: '',
+    gender: '',
+    age: '',
+    hospitalNumber: '',
+    condition: '',
+    height: '',
+    weight: '',
+    research_center: ''
+  };
 });
 </script>
 
